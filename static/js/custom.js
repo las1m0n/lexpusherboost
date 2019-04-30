275d8c6c0d4bce4ed05798c20a9e29d8c582808f
las1m0n <bondarenkonikita295@gmail.com>
2019-04-30 20:17:42 +0300

    стрелка

---
+++ static/js/custom.js
@@ -0,0 +1,22 @@
+$("#owl-demo").owlCarousel({
+    navigation : true
+  });
+
+$(".owl-carousel").owlCarousel({
+    loop:true,
+    items: 1,
+    margin:130,
+    stagePadding:30,
+    onTranslated: animateImgFunc,
+    onChanged: animateImgClear
+});
+
+//Добавляем класс с анимацией изображения
+function animateImgFunc() {
+    $(".owl-carousel .active .inner-testimonial img").addClass("animated bounceIn full-opacity");
+}
+
+//Удаляем класс с анимацией изображения
+function animateImgClear() {
+    $(".owl-carousel .active .inner-testimonial img").removeClass("animated bounceIn full-opacity");
+}
