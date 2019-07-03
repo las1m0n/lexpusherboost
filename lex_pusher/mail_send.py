639d54591bb83206b6db1bae3b7199e46770bf1c
svinerus <svinerus@gmail.com>
2019-07-03 11:25:55 +0300

    бля оно работало?

--- lex_pusher/mail_send.py
+++ lex_pusher/mail_send.py
@@ -1,12 +1,12 @@
 from django.core.mail import send_mail
-from django.conf.global_settings import EMAIL_HOST_USER
+from django.conf import settings
 
 
 def send_email(to, subject, text):
     send_mail(
         subject,
         text,
-        EMAIL_HOST_USER,
+        settings.EMAIL_HOST_USER,
         [to],
         fail_silently=False,
     )
